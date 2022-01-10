692fa13e178600deb60358a1d7d176346717c713
chaosmos <vitovich1987@gmail.com>
2022-01-11 02:04:38 +0300

    reorganized the project structure using mvc

---
+++ src/js/controller.js
@@ -0,0 +1,49 @@
+import * as model from "./model.js"
+import recipeView from "./views/recipeView.js";
+
+// const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if(!id) return;
+
+    recipeView.renderSpinner();
+
+    // 1. Loading recipe
+    await model.loadRecipe(id);
+
+    // 2. Rendering recipe
+    recipeView.render(model.state.recipe);
+
+  } catch (err) {
+    alert(err)
+  }
+}
+
+// controlRecipes();
+
+// ['hashchange', 'load'].forEach(evt => window.addEventListener(evt, controlRecipes));
+
+
+// let evtArr = ["hashchange", "load"];
+
+;["hashchange", "load"].forEach(evt => window.addEventListener(evt, controlRecipes))
+// evtArr.forEach(evt => window.addEventListener(evt, controlRecipes));
+
+// window.addEventListener("hashchange", controlRecipes);
+// window.addEventListener("load", controlRecipes);
