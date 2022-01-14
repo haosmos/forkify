afe965774863532d31dbf7405549f7b20a618c90
chaosmos <vitovich1987@gmail.com>
2022-01-15 00:07:42 +0300

    implementing search functionality. WIP

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,16 +1,10 @@
 import * as model from "./model.js"
 import recipeView from "./views/recipeView.js";
-
-// const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+import searchView from "./views/searchView.js";
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if(!id) return;
 
@@ -27,10 +21,27 @@ const controlRecipes = async function () {
   } catch (err) {
     recipeView.renderError()
   }
-}
+};
+
+const controlSearchResults = async function () {
+  try {
+    // 1. Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2. Load search results
+    await model.loadSearchResults(query);
+
+    // 3. Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 }
 
 init();
